9d4bcd6bf7b72559878789b4ed115ea91b1e8bb0
Daniel <daniel_mtz@hotmail.com>
2022-06-26 17:41:25 -0500

    Implement signin and signout logic

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -13,17 +13,40 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton() {
         if(this.state.isSignedIn === null){
-            return <div>I dont know if we are signed in</div>;
+            return null;
         }else if (this.state.isSignedIn){
-            return <div>I am signed in!</div>;
+            return(
+                <button className="ui red google button" onClick={this.onSignOut}>
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         }else {
-            return <div>I am not signed in</div>;
+            return(
+                <button className="ui red google button" onClick={this.onSignIn}>
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
