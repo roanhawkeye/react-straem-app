3c796d38a7b4d6c066f5734e700e2d70fdee7e63
Daniel <daniel_mtz@hotmail.com>
2022-06-26 17:21:06 -0500

    Identifies if the user is signed in with basic logic

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,18 +2,33 @@ import React from "react";
 import client_secrets from '../client_secrets.js';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: client_secrets.web.client_id,
                 scope: 'email',
                 plugin_name: "streamy",
-            })
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+            });
         });
     }
 
+    renderAuthButton() {
+        if(this.state.isSignedIn === null){
+            return <div>I dont know if we are signed in</div>;
+        }else if (this.state.isSignedIn){
+            return <div>I am signed in!</div>;
+        }else {
+            return <div>I am not signed in</div>;
+        }
+    }
+
     render(){
-        return <div>Google Auth</div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
 
